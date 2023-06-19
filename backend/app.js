8582a33644f79d838431a667b8f3b838ec64295b
Harsh Bharadwaj <harsh.bharadwaj.cv@gmail.com>
2023-06-19 08:39:43 +0530

    Cors update

--- backend/app.js
+++ backend/app.js
@@ -2,8 +2,10 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const cors=require('cors');
 const app = express();
 app.use(express.json());
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Database connection
